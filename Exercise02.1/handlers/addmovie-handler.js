5817f760ae81c0316abad9abdd81408c9f92220a
Nillar <martin.p.nikolov@gmail.com>
2017-10-09 21:13:23 +0300

    Meme DB problem solved

---
+++ Exercise02.1/handlers/addmovie-handler.js
@@ -0,0 +1,61 @@
+const url = require('url');
+const fs = require('fs');
+
+const db = require('../config/dataBase.js');
+
+function addMovie(req, res) {
+    let urlObj = url.parse(req.url);
+    let path = urlObj['pathname'];
+
+    if (path !== '/addMovie') return true;
+    else if (path === '/addMovie' && req['method'] === 'GET') {
+        getAddMovie(req, res);
+    } else if (path === '/addMovie' && req['method'] === 'POST') {
+        postAddMovie(req, res);
+    }
+}
+
+function getAddMovie(req, res) {
+    fs.readFile('./views/addMovie.html', function(err, data) {
+        if (err) {
+            console.log(err['message']);
+            return;
+        }
+
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
+
+        res.write(data);
+        res.end();
+    });
+}
+
+function postAddMovie(req, res) {
+    let body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        let newMovie = {  };
+        let tokens = body.split('&');
+        for (let token of tokens) {
+            let kvp = token.split('=');
+            let key = kvp[0];
+            let value = kvp[1];
+
+            newMovie[key] = value;
+        }
+
+        db.push(newMovie);
+    });
+
+    res.writeHead(301,
+        { Location: 'http://localhost:1234/addMovie' }
+    );
+
+    res.end();
+}
+
+module.exports = addMovie;
---
+++ Exercise02.1/handlers/content-handler.js
@@ -0,0 +1,45 @@
+const url = require('url');
+const fs = require('fs');
+
+function getContent(req, res) {
+    let urlObj = url.parse(req.url);
+    let path = urlObj['pathname'];
+    if (path === '/favicon.ico') {
+        path = '/public/images' + path;
+    }
+
+    if (!path.startsWith('/public')) return true;
+    else if (path.startsWith('/public')) {
+        fs.readFile('.' + path, function(err, data) {
+            if (err) {
+                console.log(err['message']);
+                return;
+            }
+
+            writeHead(path, res);
+
+            res.write(data);
+            res.end();
+        });
+    }
+}
+
+function writeHead(path, res) {
+    let dotIndex = path.indexOf('.');
+    let extenstion = path.slice(dotIndex + 1, path.length);
+
+    let mimeType = {
+        'html': 'text/html',
+        'css': 'text/css',
+        'js': 'application/javascript',
+        'png': 'image/png',
+        'jpg': 'image/jpeg',
+        'ico': 'image/x-icon'
+    };
+
+    res.writeHead(200, {
+        'content-type': mimeType[extenstion]
+    });
+}
+
+module.exports = getContent;
